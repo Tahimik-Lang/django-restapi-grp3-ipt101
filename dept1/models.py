09b32a3b96ba3ad5000bb599c5ed2336588833d4
MacMacXsus <marx.manginsay@gmail.com>
2025-05-20 12:47:52 +0800

    Fixed home dashboard analytics
    *poll style of displaying analytics
    *removed line chart since tracking is not included.

--- dept1/models.py
+++ dept1/models.py
@@ -4,6 +4,8 @@ class ChartData(models.Model):
     category = models.CharField(max_length=100)
     value = models.IntegerField()
     chart_type = models.CharField(max_length=50)  # pie, bar, etc.
+    username = models.CharField(max_length=50, default='anonymous')  # User who created the entry
+    preferred_category = models.CharField(max_length=100, blank=True)  # Optional preferred category
 
     def __str__(self):
-        return f"{self.category}: {self.value} ({self.chart_type})"
+        return f" {self.username} - {self.category}: {self.value} ({self.chart_type})"
